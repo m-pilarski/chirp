2ce185be901c0f01f6be353b4f8016e8c3c393e3
m-pilarski <89189235+m-pilarski@users.noreply.github.com>
2025-05-23 17:51:06 +0200

    update query pars

--- R/api_interaction.R
+++ R/api_interaction.R
@@ -79,28 +79,53 @@ form_tweet_query <- function(
     
     .pars_static <- list(
       tweet.fields = stringr::str_c(
-        "attachments,author_id,conversation_id,created_at,entities,geo,id,",
-        "in_reply_to_user_id,lang,public_metrics,possibly_sensitive,",
-        "referenced_tweets,source,text,withheld",
-        dplyr::if_else(.incl_context_annotations, ",context_annotations", "")
+        # "attachments,author_id,conversation_id,created_at,entities,geo,id,",
+        # "in_reply_to_user_id,lang,public_metrics,possibly_sensitive,",
+        # "referenced_tweets,source,text,withheld",
+        # dplyr::if_else(.incl_context_annotations, ",context_annotations", "")
+        "article,attachments,author_id,card_uri,community_id,",
+        "context_annotations,conversation_id,created_at,display_text_range,",
+        "edit_controls,edit_history_tweet_ids,entities,geo,id,",
+        "in_reply_to_user_id,lang,media_metadata,non_public_metrics,",
+        "note_tweet,organic_metrics,possibly_sensitive,promoted_metrics,",
+        "public_metrics,referenced_tweets,reply_settings,scopes,source,text,",
+        "withheld" 
       ),
       expansions = stringr::str_c(
-        "author_id,entities.mentions.username,geo.place_id,",
-        "in_reply_to_user_id,referenced_tweets.id,",
-        "referenced_tweets.id.author_id,attachments.media_keys"
+        # "author_id,entities.mentions.username,geo.place_id,",
+        # "in_reply_to_user_id,referenced_tweets.id,",
+        # "referenced_tweets.id.author_id,attachments.media_keys"
+        "article.cover_media,article.media_entities,attachments.media_keys,",
+        "attachments.media_source_tweet,attachments.poll_ids,author_id,",
+        "edit_history_tweet_ids,entities.mentions.username,geo.place_id,", 
+        "in_reply_to_user_id,entities.note.mentions.username,", 
+        "referenced_tweets.id,referenced_tweets.id.attachments.media_keys,",
+        "referenced_tweets.id.author_id"
+      ),
+      media.fields = stringr::str_c(
+        # "duration_ms,height,media_key,preview_image_url,",
+        # "public_metrics,type,url,width,alt_text"
+        "alt_text,duration_ms,height,media_key,non_public_metrics,",
+        "organic_metrics,preview_image_url,promoted_metrics,public_metrics,",
+        "type,url,variants,width"
+      ),
+      poll.fields = stringr::str_c(
+        "duration_minutes,end_datetime,id,options,voting_status"
       ),
       user.fields = stringr::str_c(
-        "created_at,description,entities,id,location,name,pinned_tweet_id,",
-        "profile_image_url,protected,public_metrics,url,username,verified,",
-        "withheld"
+        # "created_at,description,entities,id,location,name,pinned_tweet_id,",
+        # "profile_image_url,protected,public_metrics,url,username,verified,",
+        # "withheld"
+        "affiliation,confirmed_email,connection_status,created_at,",
+        "description,entities,id,is_identity_verified,location,",
+        "most_recent_tweet_id,name,parody,pinned_tweet_id,profile_banner_url,",
+        "profile_image_url,protected,public_metrics,receives_your_dm,",
+        "subscription,subscription_type,url,username,verified,",
+        "verified_followers_count,verified_type,withheld"
       ),
       place.fields = stringr::str_c(
         "contained_within,country,country_code,full_name,geo,id,name,place_type"
       ),
-      media.fields = stringr::str_c(
-        "duration_ms,height,media_key,preview_image_url,",
-        "public_metrics,type,url,width,alt_text"
-      )
     )
     
   }
